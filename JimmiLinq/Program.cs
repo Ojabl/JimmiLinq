17479c357ccd9ab557a1c79d605891e5acd09efe
osajab10@gmail.com <osajab10@gmail.com>
2022-09-18 16:52:51 +0200

    Add project files.

---
+++ JimmiLinq/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JimmiLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> mostExpansiveComicDescriptions =
+                from comic in Comic.Catalog
+                where Comic.Prices[comic.Issue] > 500
+                orderby Comic.Prices[comic.Issue] descending
+                select $"{comic} is worth {Comic.Prices[comic.Issue]:c}";
+        }
+    }
+}
